0414ceee4ecfd9d8c8ed83e9bef4106de43934b3
Kacper Wochniak <kacperwochniak22@gmail.com>
2025-05-12 13:21:13 +0200

    rewrite - 1.0 | post-compile batch file, moved source to src folder

---
+++ get_rel.ps1
@@ -0,0 +1,4 @@
+cd build
+mkdir bin
+Copy-Item ./Desktop_Qt_6_9_0_MinGW_64_bit-Release/TeamSpeak-Overlay.exe ./bin/TeamSpeak-Overlay.exe
+E:\Qt\6.9.0\mingw_64\bin\windeployqt6.exe ./bin/TeamSpeak-Overlay.exe
